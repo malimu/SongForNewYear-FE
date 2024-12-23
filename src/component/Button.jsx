8335a2351b59716958c72922d72028dddade007e
m-oooooonchild <syp614@ewhain.net>
2024-12-24 02:00:41 +0900

    💄 버튼 컴포넌트

---
+++ src/component/Button.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Button = ({ text, color }) => {
+  // color는 'yellow', 'brown' 둘 중 하나
+  return <Container $color={color}>{text}</Container>;
+};
+
+const Container = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: calc(100% - 6.5rem);
+  font-size: 1.875rem;
+  padding: 0.31rem 0;
+  margin: 0 3.25rem;
+  box-sizing: border-box;
+
+  background-color: ${({ $color }) =>
+    $color == 'brown' ? '#8B7966' : '#ffe4a4'};
+  color: ${({ $color }) => $color == 'brown' && 'white'};
+  border-radius: 3.125rem;
+  border: 1.5px solid #6b5743;
+`;
