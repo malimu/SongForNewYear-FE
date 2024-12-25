e3af3dcc34198c3f8187b539b6f538a793e699bf
Gabin Lee <gb94@ewhain.net>
2024-12-25 16:34:29 +0900

    💄 결과 페이지 퍼블리싱

---
+++ src/component/ResultPage/BelowContents.jsx
@@ -0,0 +1,146 @@
+import styled from 'styled-components';
+import YouTube from 'react-youtube';
+import { ImageDownloadButton } from './ImageDownloadButton';
+import { Button } from '../Button';
+import twitter from '../../assets/ResultPage/twitter_icon.svg';
+import kakao from '../../assets/ResultPage/kakao_icon.svg';
+import copylink from '../../assets/ResultPage/copylink_icon.svg';
+import spark from '../../assets/ResultPage/light_object.svg';
+
+export const BelowContents = () => {
+    return (
+        <BelowContainer>
+            <ImageDownloadButton />
+            <ShareContainer>
+                <TitleText>공유하기</TitleText>
+                <ShareIcons>
+                    <Icon src={twitter}/>
+                    <Icon src={kakao}/>
+                    <Icon src={copylink}/>
+                </ShareIcons>
+            </ShareContainer>
+            <Text>
+                지금까지 nnn명이<br />이 노래를 추천받았어요
+            </Text>
+            <TitleText>
+                바로 들어보고 싶다면
+            </TitleText>
+            <YouTubeContainer>
+                <Spark src={spark} />
+                <YouTube 
+                    videoId='YLxEK0ZKx9A'
+                    opts={{
+                        playerVars: {
+                            autoplay: 1,
+                            modestbranding: 1,
+                        },
+                    }}
+                    onEnd={(e)=>{e.target.stopVideo(0);}}
+                />
+            </YouTubeContainer>
+            <ButtonsContainer>
+                <Button text="다른 소원 빌기" color="yellow" isActive={true} />
+                <Button text="노래 전체 목록 보기" color="brown" isActive={true} />
+            </ButtonsContainer>
+        </BelowContainer>
+    );
+};
+
+const BelowContainer = styled.div`
+    @media (min-width: 501px) {
+        width: 31.25rem; // 500px
+        margin: 0 auto;
+    }
+    width: 100%;
+    margin-top: 2.56rem;
+    padding-left: 3.25rem;
+    padding-right: 3.25rem;
+    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const ShareContainer = styled.div`
+    width: 100%;
+    margin-top: 2.56rem;
+    margin-bottom: 2.88rem;
+    padding-left: 3.25rem;
+    padding-right: 3.25rem;
+    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Text = styled.div`
+    font-size: 1.5rem;
+    text-align: center;
+    margin-bottom: 1.44rem;
+    color: #ACA298;
+`;
+
+const TitleText = styled.div`
+    font-size: 2rem;
+    text-align: center;
+    margin-bottom: 1.5rem;
+`;
+
+const Icon = styled.img`
+    cursor: pointer;
+`;
+
+const ShareIcons = styled.div`
+    display: flex;
+    width: 15rem;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Spark = styled.img`
+    position: absolute;
+    top: -3.5rem;
+    right: -3.5rem;
+    z-index: 10;
+`;
+
+const YouTubeContainer = styled.div`
+    width: 100%;
+    max-width: 25rem;
+    position: relative;
+    overflow: visible;
+    background: #ACA298;
+
+    &::before {
+        content: "";
+        display: block;
+        padding-top: 56.25%; 
+        /* 16:9 비율 유지 */
+    }
+
+    iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    @media (max-width: 768px) {
+        max-width: 25rem;
+    }
+
+    @media (max-width: 480px) {
+        max-width: 20rem;
+    }
+`;
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 2.35rem;
+`;
--- src/page/ResultPage.jsx
+++ src/page/ResultPage.jsx
@@ -1,17 +1,213 @@
 import styled from 'styled-components';
+import Beginning from '../assets/ResultPage/result_beginning.svg';
+import { ImageDownloadButton } from '../component/ResultPage/ImageDownloadButton';
+import { BelowContents } from '../component/ResultPage/BelowContents';
+import { Link } from 'react-router-dom';
 
 const ResultPage = () => {
-  return <Container>결과</Container>;
+  return (
+    <Container>
+      <ResultCardContainer>
+        <ResultCard src={Beginning} />
+        <TitleContainer>
+          <Title>뷁뷁뷁뷁뷁 님의 소원</Title>
+        </TitleContainer>
+        <WishContainer>
+          <Wish>로또 1등 당첨 로또 1등 당첨 로또 1등 당첨 로또 1등 당첨 로또 1등 당첨 로또 1등 당첨</Wish>
+        </WishContainer>
+        <SongContainer>
+          <AlbumCover />
+          <SongTextContainer>
+            <SongTitle>Do You Hear The People Sing?</SongTitle>
+            <SongArtist>이찬혁</SongArtist>
+          </SongTextContainer>
+        </SongContainer>
+        <LyricsContainer>
+          <Lyrics>반짝이는 꿈들로 가득 찬 저 세상이
+          날  부르고 있잖아 조금 더 가보자</Lyrics>
+        </LyricsContainer>
+      </ResultCardContainer>
+      <SongTime>
+        12월 31일 <TimeColor>23시 59분 05초</TimeColor>에 재생 시 이 가사로 한 해를 시작할 수 있어요!
+      </SongTime>
+      <BelowContents />
+      <Footer>
+        <FooterText>
+          @ 2024 Team Malimu
+          <br />
+          All Rights Reserved
+        </FooterText>
+      </Footer>
+    </Container>
+  );
 };
 
 export default ResultPage;
 
 const Container = styled.div`
+  @media (min-width: 500px) {
+    width: 31.25rem; // 500px
+    margin: 0 auto;
+  }
+  width: 100%;
   min-height: 100vh;
   background-color: var(--beige);
 
-  @media (min-width: 500px) {
-    width: 31.25rem; //500px
-    margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
   }
+`;
+
+const ResultCardContainer = styled.div`
+  position: relative;
+  width: 100%;
+  @media (min-width: 500px) {
+    width: 31.25rem; // 500px
+  }
+  min-width: 360px;
+  height: auto;
+`;
+
+const ResultCard = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const TitleContainer = styled.div`
+  position: absolute;
+  top: 19%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 2.5rem 0;
+  box-sizing: border-box;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-size: clamp(2rem, 1.5rem + 2vw, 3rem);
+`;
+
+const WishContainer = styled.div`
+  position: absolute;
+  top: 44%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 70%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 2.5rem 0;
+  box-sizing: border-box;
+`;
+
+const Wish = styled.div`
+  text-align: center;
+  font-size: clamp(1.5rem, 1.5rem + 2vw, 4rem);
+`;
+
+const SongContainer = styled.div`
+  position: absolute;
+  top: 67%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 2.5rem 0;
+  box-sizing: border-box;
+`;
+
+const AlbumCover = styled.img`
+  width: clamp(5rem, 5rem + 5vw, 10rem);
+  height: clamp(5rem, 5rem + 5vw, 10rem);
+  border-radius: 0.625rem;
+  background: url(<path-to-image>) #FFE4A4 50% / cover no-repeat;
+`;
+
+const SongTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 20rem;
+  margin-left: 1rem;
+`;
+
+const SongTitle = styled.div`
+  font-size: clamp(1rem, 1rem + 3vw, 4rem);
+  margin-bottom: 0.69rem;
+`;
+
+const SongArtist = styled.div`
+  font-size: clamp(1rem, 1rem + 3vw, 4rem);
+`;
+
+const LyricsContainer = styled.div`
+  position: absolute;
+  top: 82%;
+  width: 100%;
+  padding: 0rem 4rem;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+`;
+
+const Lyrics = styled.div`
+  text-align: center;
+  font-size: clamp(1rem, 1rem + 3vw, 4rem);
+`;
+
+const SongTime = styled.div`
+  text-align: center;
+  width: 100%;
+  padding: 0rem 5rem;
+  box-sizing: border-box;
+  font-size: clamp(0.7em, 0.7rem + 3vw, 4rem);
+`;
+
+const TimeColor = styled.div`
+  color: #2C5EA7;
+  display: inline;
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  height: 18.12rem;
+  position: relative;
+
+  overflow-x: hidden;
+  overflow-y: hidden;
+`;
+
+const FooterText = styled.div`
+  color: var(--browngray);
+  font-size: 1.25rem;
+  text-align: center;
+
+  position: absolute;
+  transform: translate(-50%, -50%);
+
+  left: 50%;
+  bottom: 0;
+  z-index: 10;
 `;
